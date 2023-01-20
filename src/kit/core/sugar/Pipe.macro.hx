62926f8695bcd726fbc6d34c5b548be09d8c3ce9
wartman <faxmaster@gmail.com>
2023-01-20 10:51:45 -0600

    Wait this can be much simpler

--- src/kit/core/sugar/Pipe.macro.hx
+++ src/kit/core/sugar/Pipe.macro.hx
@@ -4,15 +4,11 @@ import haxe.macro.Expr;
 import haxe.macro.Context;
 
 function createPipe(exprs:Array<Expr>) {
-	var first = exprs.shift();
-	var body:Array<Expr> = [];
-	var tmp = '__pipe';
+	var body = exprs.shift();
 
-	if (first == null) return macro null;
+	if (body == null) return macro null;
 
-	body.push(macro var $tmp = $first);
-
-	for (index => expr in exprs) switch expr.expr {
+	for (expr in exprs) switch expr.expr {
 		case ECall(e, params):
 			var slot:Null<Int> = null;
 			for (index => param in params) switch param.expr {
@@ -25,24 +21,16 @@ function createPipe(exprs:Array<Expr>) {
 			if (slot == null) {
 				Context.error('Slot required', expr.pos);
 			}
-			params[slot] = macro $i{tmp};
-			tmp = '__pipe$index';
-			body.push(macro var $tmp = $expr);
+			params[slot] = macro $body;
+			body = expr;
 		case EFunction(kind, f):
 			if (f.args.length != 1) {
 				Context.error('Only functions with one argument are allowed here.', expr.pos);
 			}
-			var call = macro($expr)($i{tmp});
-			tmp = '__pipe$index';
-			body.push(macro var $tmp = $call);
+			body = macro($expr)($body);
 		default:
 			Context.error('Invalid expression', expr.pos);
 	}
 
-	trace(haxe.macro.ExprTools.toString(macro $b{body}));
-
-	return macro {
-		@:mergeBlock $b{body};
-		$i{tmp};
-	}
+	return body;
 }
