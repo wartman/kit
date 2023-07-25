e92af4f4d11377f5ddb5a88e81d833d726498a0d
wartman <faxmaster@gmail.com>
2023-07-25 13:41:16 -0500

    use Exception for error

--- src/kit/Error.hx
+++ src/kit/Error.hx
@@ -1,7 +1,6 @@
 package kit;
 
 import haxe.Exception;
-import haxe.PosInfos;
 
 enum abstract ErrorCode(Int) from Int {
 	final BadRequest = 400;
@@ -22,39 +21,11 @@ enum abstract ErrorCode(Int) from Int {
 	final LoopDetected = 508;
 }
 
-typedef ErrorObject = {
+class Error extends Exception {
 	public final code:ErrorCode;
-	public final message:String;
-}
 
-// @todo: Unsure about this one. Should it extend Exception?
-// Is it even needed? We're just copying Tink here because I found
-// Errors and their ErrorCodes to be useful, but maybe there's a
-// better way.
-
-@:forward(code, message)
-abstract Error(ErrorObject) {
-	@:from public static function ofException(e:Exception) {
-		return new Error(InternalError, e.toString());
-	}
-
-	public inline function new(code, message) {
-		this = {code: code, message: message};
-	}
-
-	@:to public function toResult<T>():Result<T> {
-		return Error(new Error(this.code, this.message));
-	}
-
-	@:to public function toString():String {
-		return '${this.code} ${this.message}';
-	}
-
-	public function asException(?pos:PosInfos) {
-		return new Exception(toString(), pos);
-	}
-
-	public inline function throwError(?pos:PosInfos) {
-		throw asException(pos);
+	public function new(code, message) {
+		super(message);
+		this.code = code;
 	}
 }
